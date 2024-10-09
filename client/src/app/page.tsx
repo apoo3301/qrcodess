3e133cfe4cf0948151cfeddb21b301f9ab5a8b07
Menzo SMIT <Menzo.SMIT@igymarinas.com>
2024-10-09 15:54:54 +0200

    .png fix

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -206,7 +206,7 @@ export default function Home() {
     <div className="flex flex-col min-h-[100dvh] overflow-hidden">
       <header className="fixed top-0 left-0 w-full px-6 lg:px-8 h-16 flex items-center justify-between bg-white shadow-md border-b border-gray-200 z-50">
         <Link href="#" className="flex items-center space-x-3" prefetch={false}>
-          <Image src="/igyBlackicon.svg" alt="IGYICON" width={100} height={100} className="transition-transform transform hover:scale-105" />
+          <Image src="/igyBlackicon.png" alt="IGYICON" width={100} height={100} className="transition-transform transform hover:scale-105" />
           <span className="text-xl font-semibold tracking-wide transition-colors">
             Customer Pass Manager
           </span>
